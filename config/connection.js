bf58f586ce0582baf54582a6f020f391b89df3ed
Thecanadianchris <thecanadianchris@yahoo.com>
2026-07-15 10:07:47 +0100

    added database

---
+++ config/connection.js
@@ -0,0 +1,20 @@
+
+require("dotenv").config();
+
+const Sequelize = require("sequelize");
+
+// use the hosted database url if it exists (for Render), otherwise use the local .env settings
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(
+      process.env.DB_DATABASE,
+      process.env.DB_USERNAME,
+      process.env.DB_PASSWORD,
+      {
+        host: process.env.DB_HOST,
+        dialect: process.env.DB_DIALECT,
+        port: process.env.DB_PORT,
+      }
+    );
+
+module.exports = sequelize;
